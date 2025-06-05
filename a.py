d195bcaa0e70fa0ec161401a49cdf123139a64e8
zerodayip <duymaz.mrt@gmail.com>
2025-06-06 02:05:56 +0700

    Update a.py

--- a.py
+++ a.py
@@ -6,20 +6,15 @@ import time
 # Mediafire linkini buraya gir
 mediafire_url = "https://www.mediafire.com/file/8lyizr4y1l5v91u/zuihou-de-zhaohuan-shi-11-bolum.mp4/file"
 
-# (Eğer sadece konsolda görünmesini istersen indir klasörü ayarlamana gerek yok)
 options = Options()
-options.add_argument("--headless")  # Tarayıcıyı görünmez açmak için (istemiyorsan kaldırabilirsin)
+options.add_argument("--headless")  # Tarayıcıyı görünmez açmak için (istemezsen kaldırabilirsin)
 driver = webdriver.Chrome(options=options)
 
 driver.get(mediafire_url)
-time.sleep(5)  # Sayfa tamamen yüklensin diye biraz bekle
+time.sleep(5)  # Sayfa tamamen yüklensin
 
-try:
-    # Download butonunu bul
-    download_button = driver.find_element(By.CSS_SELECTOR, 'a#downloadButton')
-    real_link = download_button.get_attribute("href")
-    print("Gerçek indirme linki:", real_link)
-except Exception as e:
-    print("Download linki bulunamadı!", e)
+# Tüm HTML'yi al ve yazdır
+html = driver.page_source
+print(html)
 
 driver.quit()
