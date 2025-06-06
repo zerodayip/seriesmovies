1e37ffd8d899347057bc03dc17a62b266a9cdfe6
zerodayip <duymaz.mrt@gmail.com>
2025-06-07 05:43:56 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://aniworld.to/anime/stream/from-old-country-bumpkin-to-master-swordsman"
+base_url = "https://aniworld.to"
+page_url = "https://aniworld.to/anime/stream/from-old-country-bumpkin-to-master-swordsman"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0"
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(page_url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# "Episoden" başlıklı <ul> içindeki <a> taglarını bulalım
 episodes_ul = None
 for ul in soup.select("div.hosterSiteDirectNav#stream ul"):
     if ul.find("span") and "Episoden" in ul.find("span").text:
@@ -19,6 +19,7 @@ for ul in soup.select("div.hosterSiteDirectNav#stream ul"):
 if episodes_ul:
     episode_links = episodes_ul.find_all("a")
     for a in episode_links:
-        print(a.get("href"))
+        abs_link = requests.compat.urljoin(base_url, a.get("href"))
+        print(abs_link)
 else:
     print("Episoden listesi bulunamadı!")
