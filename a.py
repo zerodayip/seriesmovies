22161602f6192ffa235c221bcbe78570c3e8a1ee
zerodayip <duymaz.mrt@gmail.com>
2025-06-10 22:46:08 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.setfilmizle.nl/film/karantina/"
+# Hedef video linki
+url = "https://vctplay.site/video/lMWDCxaHOSVZ"
 
+# Gerekli headers (Referer + User-Agent)
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+    "Referer": "https://setfilmizle.nl"
 }
 
+# İstek gönder
 response = requests.get(url, headers=headers)
 
+# Duruma göre yanıtla
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.prettify())  # Tam HTML çıktısını yazdırır
+    print(soup.prettify())  # veya print(soup.title)
 else:
     print(f"❌ HTTP {response.status_code} - Sayfa alınamadı.")
