e572f8768def1d7092b5a98f6b76a09aabbd067f
zerodayip <duymaz.mrt@gmail.com>
2025-06-10 22:41:18 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://filmmakinesi.de/film/amator-izle-2025-fm1/"
+url = "https://www.setfilmizle.nl/film/karantina/"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
