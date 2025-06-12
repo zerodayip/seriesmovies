d6be0c6d36be9bbeacb2be754a402dd454a48f8e
zerodayip <duymaz.mrt@gmail.com>
2025-06-13 03:22:33 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,13 +1,13 @@
 import requests
 
 # Hedef video sayfası
-url = "https://epikplayer.xyz/embed/mL2OoDVV1MN4yYI/"
+url = "https://d2.premiumvideo.click/player.php?file_id=2dn66q6d3mosqg8gr4xr"
 
 # Gerçek bir tarayıcı gibi davranan header'lar
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Referer": "https://hdfilmizlesen.com",  # Ana sayfa genellikle referer olarak kullanılır
+    "Referer": "https://dizifun3.com",  # Ana sayfa genellikle referer olarak kullanılır
     "X-Requested-With": "XMLHttpRequest"   # AJAX isteği gibi görünmesini sağlar (bazı sunucular bunu ister)
 }
 
