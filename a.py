79a6777118e47d7e80a00dcfca2339a818efd4a0
zerodayip <duymaz.mrt@gmail.com>
2025-06-13 02:16:14 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,13 +1,13 @@
 import requests
 
 # Hedef video sayfası
-url = "https://filmdefilm.xyz/video/24e01830d213d75deb99c22b9cd91ddd"
+url = "https://zupeo.com/v/7jeGT46THUkg/"
 
 # Gerçek bir tarayıcı gibi davranan header'lar
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Referer": "https://filmdefilm.xyz/",  # Ana sayfa genellikle referer olarak kullanılır
+    "Referer": "https://jetfilmizle.watch/",  # Ana sayfa genellikle referer olarak kullanılır
     "X-Requested-With": "XMLHttpRequest"   # AJAX isteği gibi görünmesini sağlar (bazı sunucular bunu ister)
 }
 
